61fb84c93640951c3d93062c87e743f7df3af470
Ewa Szalwinska <e.szalwinska@gmail.com>
2026-01-22 17:01:34 -0800

    professors.js queries has been created

--- db/queries/professors.js
+++ db/queries/professors.js
@@ -1,10 +1,46 @@
 import db from "#db/client";
 
 export async function createProfessor(name, bio, email) {
-    const sql = `
+  const sql = `
     INSERT INTO professors
     (name, bio, email)
     VALUES
     ($1, $2, $3)
     RETURNING *`;
+
+  const {
+    rows: [professor],
+  } = await db.query(sql, [name, bio, email]);
+  return professor;
+}
+
+export async function getAllProfessors() {
+  const sql = `SELECT * FROM professors`;
+  const { rows: professors } = await db.query(sql);
+  return professors;
+}
+
+export async function getProfessorById(id) {
+  const sql = `SELECT * FROM professors WHERE id = $1`;
+  const {
+    rows: [professor],
+  } = await db.query(sql, [id]);
+  return professor;
+}
+
+export async function updateProfessor(id, name, bio, email) {
+  const sql = `UPDATE professors SET name = $2, bio = $3, email = $4 WHERE id = $1 RETURNING *`;
+
+  const {
+    rows: [professor],
+  } = await db.query(sql, [id, name, bio, email]);
+  return professor;
+}
+
+export async function deleteProfessorById(id) {
+  const sql = `DELETE FROM professors WHERE id = $1 RETURNING *`;
+  const {
+    rows: [professor],
+  } = await db.query(sql, [id]);
+  return professor;
 }
